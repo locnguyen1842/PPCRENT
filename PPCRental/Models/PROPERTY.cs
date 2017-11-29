657bca17d9fd7bdb51ae109902ab5451f5ede2b7
Loc Nguyen <haimuoibon024@gmail.com>
2017-11-29 21:40:15 +0700

    repair model

--- PPCRental/Models/PROPERTY.cs
+++ PPCRental/Models/PROPERTY.cs
@@ -12,7 +12,6 @@ namespace PPCRental.Models
     using System;
     using System.Collections.Generic;
     using System.Web;
-    
     public partial class PROPERTY
     {
         public PROPERTY()
@@ -51,6 +50,7 @@ namespace PPCRental.Models
         public virtual USER USER { get; set; }
         public virtual USER USER1 { get; set; }
         public virtual WARD WARD { get; set; }
+
         public HttpPostedFileBase AvatarUpload { get; set; }
         public IEnumerable<HttpPostedFileBase> Up { get; set; }
     }
